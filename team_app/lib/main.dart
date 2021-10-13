1257023523518f3df2fa6341edf5ea36b725ef1a
Kkunming <jirutcha-vim63@tbs.tu.ac.th>
2021-10-13 10:33:43 +0700

    team_app update

--- team_app/lib/main.dart
+++ team_app/lib/main.dart
@@ -1,50 +1,153 @@
-
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/screens/details_screen.dart';
+import 'package:meditation_app/widgets/bottom_nav_bar.dart';
+import 'package:meditation_app/widgets/category_card.dart';
+import 'package:meditation_app/widgets/search_bar.dart';
 
-import 'Pages/home_page.dart';
-import 'Pages/data_display.dart';
-import 'Pages/exercise_history.dart';
-import 'Pages/input_data.dart';
-import 'models/first_form_model.dart';
+import 'exercise screens/running_screen.dart';
+import 'exercise screens/swimming_screen.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FirstFormModel(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Meditation App',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey[700],
-        primaryColor: Colors.grey[850],
-        accentColor: Colors.tealAccent[400],
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.cyanAccent),
-        ),
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
-      initialRoute: '/5',
-      routes: <String, WidgetBuilder> {
-        '/1': (context) => DataDisplay(),
-        '/2': (context) => InputData(),
-        '/4': (context) => ExerciseHistory(),
-        '/5': (context) => HomePage(),
-      }
+      home: HomeScreen(),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            // Here the height of the container is 45% of our total height
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFF62CB3B),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF383C40),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Good Mornign \nUser",
+                    style: Theme.of(context)
+                        .textTheme
+                        .display1
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  SearchBar(),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        
+                        CategoryCard(
+                          title: "Running",
+                          svgSrc: "assets/icons/Hamburger.svg",
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return RunningScreen();
+                              }),
+                            );
+                          },
+                        ),
+                        
+                        CategoryCard(
+                          title: "Swimming",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return SwimmingScreen();
+                              }),
+                            );
+                          },
+                        ),
+                        
+                        CategoryCard(
+                          title: "Meditation",
+                          svgSrc: "assets/icons/Meditation.svg",
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return DetailsScreen();
+                              }),
+                            );
+                          },
+                        ),
+                        
+                        CategoryCard(
+                          title: "Yoga",
+                          svgSrc: "assets/icons/yoga.svg",
+                          press: () {},
+                        ),
+                        
+                        CategoryCard(
+                          title: "Boxing",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                        ),
+
+                        CategoryCard(
+                          title: "Cardio",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
