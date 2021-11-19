1119abc0237a0c2a5e535c17eb5444dc7d502706
Kkunming <jirutcha-vim63@tbs.tu.ac.th>
2021-11-19 23:37:57 +0700

    update 19 nov

--- team_app/lib/main.dart
+++ team_app/lib/main.dart
@@ -1,14 +1,32 @@
+import 'package:exercise_app/screens/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:workout_app/screens/welcom_view.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      title: 'Exercise App',
       debugShowCheckedModeBanner: false,
-      home: WelcomView(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
     );
   }
 }
